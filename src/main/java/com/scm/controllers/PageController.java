46a55405101685979d5b7f35c723d0ae7af6fac0
Vishnu Thakur <vishnuthakur74260@gmail.com>
2024-06-23 22:17:05 +0545

    form validation completed

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -63,13 +65,21 @@ public class PageController {
     }
 
     @PostMapping("/do_register")
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session){
+    public String processRegister(@Valid @ModelAttribute UserForm userForm,BindingResult rBindingResult, HttpSession session){
         System.out.println("process register...");
 
         //fetch form data
         //userform
         System.out.println(userForm);
         //validate form data
+        if(rBindingResult.hasErrors()){
+            return "signup";
+        }
+
+
+
+
+
         // Todo 
 
         //save to database
--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,11 +1,15 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.StandardException;
 
 @Getter
 @Setter
@@ -15,9 +19,18 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Min 3 and Max 20 character is required")
     private String name;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be 6 characters long")
     private String password;
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
     private String email;
+    @Size(min = 8, max = 10, message = "Invalid Number")
     private String phoneNumber;
+    @NotBlank(message = "About is required")
     private String about;
 }
